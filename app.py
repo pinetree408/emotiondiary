ba4f59d470843e53f64e18acd2d5aee6f508e591
leesangyoon <pinetree408@gmail.com>
2017-06-16 14:23:00 +0900

    Fix code convention without documentize & flask denpendent items

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, g, render_template, redirect, url_for, session, request
-from flask_oauth import OAuth
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.ext import mutable
+import json
+import random
 from os import path
 from datetime import datetime, date, time
 import facebook_info
 
-import sqlite3
-import json
-import random
+from flask import Flask, render_template, redirect, url_for, session, request
+from flask_oauth import OAuth
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext import mutable
 
 SECRET_KEY = 'development key'
 DEBUG = True
@@ -21,8 +20,8 @@ app.secret_key = SECRET_KEY
 oauth = OAuth()
 
 BASE_DIRECTORY = path.abspath(path.dirname(__file__))
-dbURL = '{0}{1}'.format('sqlite:///', path.join(BASE_DIRECTORY, 'emotiondiary.db'))
-app.config['SQLALCHEMY_DATABASE_URI'] = dbURL
+DB_URL = '{0}{1}'.format('sqlite:///', path.join(BASE_DIRECTORY, 'emotiondiary.db'))
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
@@ -31,17 +30,15 @@ class JsonEncodedDict(db.TypeDecorator):
     """Enables JSON storage by encoding and decoding on the fly."""
     impl = db.Text
 
-    def process_bind_param(self, value, dialect):
+    def process_bind_param(self, value):
         if value is None:
             return '{}'
-        else:
-            return json.dumps(value)
+        return json.dumps(value)
 
-    def process_result_value(self, value, dialect):
+    def process_result_value(self, value):
         if value is None:
             return {}
-        else:
-            return json.loads(value)
+        return json.loads(value)
 
 mutable.MutableDict.associate_with(JsonEncodedDict)
 
@@ -71,7 +68,7 @@ class User(db.Model):
 
 class Tip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.Integer) 
+    number = db.Column(db.Integer)
     locale = db.Column(db.String(2))
     tip = db.Column(db.Text)
     cite = db.Column(db.Text)
@@ -99,33 +96,37 @@ class Game(db.Model):
 db.create_all()
 
 facebook = oauth.remote_app('facebook',
-    base_url='https://graph.facebook.com/',
-    request_token_url=None,
-    access_token_url='/oauth/access_token',
-    authorize_url='https://www.facebook.com/dialog/oauth',
-    consumer_key=FACEBOOK_APP_ID,
-    consumer_secret=FACEBOOK_APP_SECRET,
-    request_token_params={'scope': 'email'}
-)
+                            base_url='https://graph.facebook.com/',
+                            request_token_url=None,
+                            access_token_url='/oauth/access_token',
+                            authorize_url='https://www.facebook.com/dialog/oauth',
+                            consumer_key=FACEBOOK_APP_ID,
+                            consumer_secret=FACEBOOK_APP_SECRET,
+                            request_token_params={'scope': 'email'}
+                           )
 
 def tip_builder():
-    tipFile = open('static/txt/tips.txt', 'r')
-    tipLines = tipFile.readlines()
+    tip_file = open('static/txt/tips.txt', 'r')
+    tip_lines = tip_file.readlines()
 
-    for tipLine in tipLines:
-        splittedTip = tipLine.split('\t')
-        if len(splittedTip) == 1:
+    for tip_line in tip_lines:
+        splitted_tip = tip_line.split('\t')
+        if len(splitted_tip) == 1:
             continue
-        choices = [item.strip().decode('utf-8') for item in splittedTip[7:] if len(item.strip()) != 0]
-        tip_object = Tip(number=splittedTip[0].strip(),
-                        locale=splittedTip[1].strip(),
-                        tip=splittedTip[2].strip().decode('utf-8'),
-                        cite=splittedTip[3].strip(),
-                        url=splittedTip[4].strip(),
-                        quotation=splittedTip[5].strip().decode('utf-8'),
-                        question=splittedTip[6].strip().decode('utf-8'),
-                        answer=splittedTip[7].strip().decode('utf-8'),
-                        choices=choices
+        choices = []
+        for item in splitted_tip[7:]:
+            if item.strip():
+                choices.append(item.strip().decode('utf-8'))
+
+        tip_object = Tip(number=splitted_tip[0].strip(),
+                         locale=splitted_tip[1].strip(),
+                         tip=splitted_tip[2].strip().decode('utf-8'),
+                         cite=splitted_tip[3].strip(),
+                         url=splitted_tip[4].strip(),
+                         quotation=splitted_tip[5].strip().decode('utf-8'),
+                         question=splitted_tip[6].strip().decode('utf-8'),
+                         answer=splitted_tip[7].strip().decode('utf-8'),
+                         choices=choices
                         )
         db.session.add(tip_object)
         db.session.commit()
@@ -136,15 +137,15 @@ tip_builder()
 def index():
     if session.get('user'):
         user = session.get('user')
-        return render_template('index.html', user = user)
+        return render_template('index.html', user=user)
     return redirect(url_for('login'))
 
 @app.route("/calendar")
 def calendar():
     user = session.get('user')
-    today = datetime.combine(date.today(), time(0,0,0))
-    calendar = Calendar.query.filter_by(user_id=user['id'], pub_date=today).first()
-    if not calendar:
+    today = datetime.combine(date.today(), time(0, 0, 0))
+    calendar_info = Calendar.query.filter_by(user_id=user['id'], pub_date=today).first()
+    if not calendar_info:
         return redirect(url_for('calendar_create'))
     calendar_objects = Calendar.query.filter_by(user_id=user['id']).all()
     calendar_list = list(map(lambda c_object: c_object.as_dict(), calendar_objects))
@@ -158,11 +159,17 @@ def calendar_create():
         emotion = int(json_data['emotion'])
         degree = int(json_data['degree'])
         message = json_data['message']
-        today = datetime.combine(date.today(), time(0,0,0))
-        calendar = Calendar(content=message, emotion=emotion, degree=degree, pub_date=today, user_id=user['id'])
-        db.session.add(calendar)
+        today = datetime.combine(date.today(), time(0, 0, 0))
+        calendar_item = Calendar(content=message,
+                                 emotion=emotion,
+                                 degree=degree,
+                                 pub_date=today,
+                                 user_id=user['id'])
+        db.session.add(calendar_item)
         db.session.commit()
-        return json.dumps({'success':True, 'redirect': '/calendar'}), 200, {'ContentType':'application/json'}
+        return (json.dumps({'success':True, 'redirect': '/calendar'}),
+                200,
+                {'ContentType':'application/json'})
     emotion_contents = [
         ("Very Bad", "img/calendar/emotion-2.png"),
         ("Bad", "img/calendar/emotion-1.png"),
@@ -178,21 +185,23 @@ def calendar_create():
         ("Very Strong", "img/calendar/degree5.png")
     ]
     message = [
-        "Write down today's or past major events and events that have had the greatest impact on your mood today.",
+        "Write down today's or past major events and events \
+                that have had the greatest impact on your mood today.",
         "Please feel free to write your feelings about the above events, work or thoughts.",
-        "You wrote about today's events, work and feelings. If you have words of praise, encouragement, understanding, enlightenment that you can give to yourself in this regard, please write it freely."
+        "You wrote about today's events, work and feelings. \
+                If you have words of praise, encouragement, understanding, enlightenment \
+                that you can give to yourself in this regard, please write it freely."
     ]
     return render_template('calendar/create/index.html',
-        emotion_contents=emotion_contents,
-        emotion_degrees=emotion_degrees,
-        message=message
-    )
+                           emotion_contents=emotion_contents,
+                           emotion_degrees=emotion_degrees,
+                           message=message
+                          )
 
-@app.route("/calendar/emotion/<int:id>", methods=['GET', 'POST'])
-def calendar_emotion(id):
-    user = session.get('user')
-    calendar = Calendar.query.filter_by(id=id).first()
-    calendar_info = json.loads(calendar.as_dict())
+@app.route("/calendar/emotion/<int:calendar_id>", methods=['GET', 'POST'])
+def calendar_emotion(calendar_id):
+    calendar_item = Calendar.query.filter_by(id=calendar_id).first()
+    calendar_info = json.loads(calendar_item.as_dict())
     return render_template('calendar/detail.html', calendar_info=calendar_info)
 
 @app.route("/tips", methods=['GET', 'POST'])
@@ -202,10 +211,10 @@ def tips():
         number = request.form['tip_number']
         locale = request.form['tip_locale']
         tip = Tip.query.filter_by(number=int(number), locale=locale).first()
-	if tip.answer == answer:
+        if tip.answer == answer:
             return render_template('tip/correct.html')
         return render_template('tip/wrong.html')
-    tip = Tip.query.filter_by(locale='KR',number=random.randint(1, 10)).first()
+    tip = Tip.query.filter_by(locale='KR', number=random.randint(1, 10)).first()
     return render_template('tip/index.html', tips=tip)
 
 @app.route("/tests", methods=["GET", "POST"])
@@ -214,20 +223,19 @@ def tests():
     if request.method == "POST":
         score = []
         for i in range(20):
-            scoreItem = eval("request.form.get('var" + str(i) + "')")
-            if scoreItem:
-                score.append(int(scoreItem))
+            score_item = eval("request.form.get('var" + str(i) + "')")
+            if score_item:
+                score.append(int(score_item))
         scoresum = int(sum(score))
-        today = datetime.combine(date.today(), time(0,0,0))
-        calendar = Test(category="ces-d", result=scoresum, pub_date=today, user_id=user['id'])
-        db.session.add(calendar)
+        today = datetime.combine(date.today(), time(0, 0, 0))
+        test_item = Test(category="ces-d", result=scoresum, pub_date=today, user_id=user['id'])
+        db.session.add(test_item)
         db.session.commit()
         if scoresum < 10:
             return render_template("tests/feedback1.html", user_name=user['name'])
-        elif 10 <= scoresum < 21: 
+        elif 10 <= scoresum < 21:
             return render_template("tests/feedback2.html", user_name=user['name'])
-        else:
-            return render_template("tests/feedback3.html", user_name=user['name'])
+        return render_template("tests/feedback3.html", user_name=user['name'])
     return render_template("tests/ces-d.html")
 
 @app.route("/game", methods=["GET", "POST"])
@@ -235,9 +243,9 @@ def game():
     user = session.get('user')
     if request.method == 'POST':
         result = request.form['result']
-        today = datetime.combine(date.today(), time(0,0,0))
-        game = Game(result=int(result), pub_date=today, user_id=user['id'])
-        db.session.add(game)
+        today = datetime.combine(date.today(), time(0, 0, 0))
+        game_item = Game(result=int(result), pub_date=today, user_id=user['id'])
+        db.session.add(game_item)
         db.session.commit()
         return redirect(url_for('game'))
     return render_template("game/stroop.html")
@@ -252,9 +260,10 @@ def about():
 
 @app.route('/login')
 def login():
-    return facebook.authorize(callback=url_for('facebook_authorized',
-        next=request.args.get('next') or request.referrer or None,
-        _external=True))
+    return facebook.authorize(
+        callback=url_for('facebook_authorized',
+                         next=request.args.get('next') or request.referrer or None,
+                         _external=True))
 
 @app.route('/login/authorized')
 @facebook.authorized_handler
@@ -265,13 +274,13 @@ def facebook_authorized(resp):
             request.args['error_description']
         )
     session['oauth_token'] = (resp['access_token'], '')
-    me = facebook.get('/me')
+    me_info = facebook.get('/me')
     # For test
     if session.get('user'):
         return redirect(url_for('index'))
-    user = User.query.filter_by(facebookID=me.data['id']).first()
+    user = User.query.filter_by(facebookID=me_info.data['id']).first()
     if not user:
-        user = User(facebookID=str(me.data['id']), name=me.data['name'])
+        user = User(facebookID=str(me_info.data['id']), name=me_info.data['name'])
         db.session.add(user)
         session['user'] = dict(id=user.id, name=user.name, facebookID=user.facebookID)
     db.session.commit()
